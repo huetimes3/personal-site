ba203dc599a78c0319a64daa610dee3643e3bb0e
huetimes3 <ttricamavrodin@gmail.com>
2024-11-14 23:54:48 -0500

    added very simple mobile mode and color toggle

--- home/script.js
+++ home/script.js
@@ -1,8 +1,20 @@
 // set attribute for the page depending on if browser is in dark mode or not
 document.addEventListener('DOMContentLoaded', function() {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-      document.documentElement.setAttribute('data-theme', 'light');
-    }
-  });
+	var isDarkDefault = window.matchMedia('(prefers-color-scheme: dark)').matches;
+	if (isDarkDefault) {
+	  	document.documentElement.setAttribute('data-theme', 'dark');
+	} else {
+	  	document.documentElement.setAttribute('data-theme', 'light');
+	}
+	var darkModeToggle = document.getElementById('dark-mode-toggle');
+	//either link to moon or sun svg icon
+	darkModeToggle.innerHTML = isDarkDefault ? '<img src="assets/sun_icon.svg" alt="light mode">' : '<img src="assets/moon_icon.svg" alt="dark mode">';
+	darkModeToggle.addEventListener('click', toggleDarkMode);
+});
+
+function toggleDarkMode() {
+	const currentTheme = document.documentElement.getAttribute('data-theme');
+	const switchToTheme = currentTheme === 'dark' ? 'light' : 'dark';
+	document.getElementById('dark-mode-toggle').innerHTML = switchToTheme === 'dark' ? '<img src="assets/sun_icon.svg" alt="light mode">' : '<img src="assets/moon_icon.svg" alt="dark mode">';
+	document.documentElement.setAttribute('data-theme', switchToTheme);
+}
